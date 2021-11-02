bf81b412f44e33cbdaf1d9b7b55b13ffaddf99e8
dominthao91 <domintaopt@gmail.com>
2021-11-02 11:28:36 +0700

    TH view

--- src/ChimucMSQL.sql
+++ src/ChimucMSQL.sql
@@ -80,3 +80,16 @@ call setCounter(@counter,1);
 call setCounter(@counter,1);
 call setCounter(@counter,5);
 select @counter;
+
+# View trong MySql
+create view  customer_views as select customerNumber,customerName,phone
+from customers;
+select * from  customer_views;
+CREATE OR REPLACE VIEW customer_views AS
+
+SELECT customerNumber, customerName, contactFirstName, contactLastName, phone
+
+FROM customers
+
+WHERE city = 'Nantes';
+DROP VIEW customer_views;
